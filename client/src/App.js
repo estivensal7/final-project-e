cab534b71af5c75585df159a256eef0b7036aae6
estivensal7 <estivensal7@gmail.com>
2020-05-14 21:15:24 -0400

    routes work

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 import API from "./utils/api";
 
 class App extends React.Component {
-	componentDidMount() {
-		API.saveDonations({
-			eventName: "Name of New Event",
-			eventDate: "2020-05-17T01:08:49.643Z",
-			lunchNumber: 300,
-			eventDescription:
-				"These lunches will be available from 11am-2pm, Address Here/Phonenumber",
-			userName: "sampleUsername2",
-		}).then((res) => console.log(res));
-		// API.allDonations().then((res) => console.log(res.data));
-	}
+	// componentDidMount() {
+
+	// API.saveDonations({
+	// 	eventName: "Name of New Event",
+	// 	eventDate: "2020-05-17T01:08:49.643Z",
+	// 	lunchNumber: 300,
+	// 	eventDescription:
+	// 		"These lunches will be available from 11am-2pm, Address Here/Phonenumber",
+	// 	userName: "sampleUsername2",
+	// }).then((res) => console.log(res));
+	// API.allDonations().then((res) => console.log(res.data));
+
+	// }
 
 	render() {
 		return (
